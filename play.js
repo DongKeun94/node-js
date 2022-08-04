bde98cb9447a34b15394521ae09740c032b950ea
dongkeun94 <donggen94@pukyong.ac.kr>
2022-08-04 20:41:47 +0900

    first

---
+++ play.js
@@ -0,0 +1,9 @@
+var name = 'kuhn';
+var age = 29;
+var hashobbies = true;
+
+function summarizeUser(userName,userAge,userHobby){
+    return `Name is ${userName}, age is ${age} and the user has hobbies ${userHobby}`;
+};
+
+console.log(summarizeUser(name,age,hashobbies));
